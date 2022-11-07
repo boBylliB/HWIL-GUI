3f00a412b40ad209dc93d2cad265832dcadde139
spinnerSAA <73348803+spinnerSAA@users.noreply.github.com>
2022-11-07 07:53:13 -0700

    Detailed Comments

--- GUIVersion0.py
+++ GUIVersion0.py
@@ -10,6 +10,7 @@ import datetime
 import threading
 import socket
 
+# class for the GUI itself
 class MainPage(ttk.Frame):
 	def __init__(self, *args, **kwargs):
 		super().__init__(**kwargs)
@@ -29,30 +30,42 @@ class MainPage(ttk.Frame):
 #	def changeState(self,text):
 #		self.state_lbl['text'] = text
 
+
+	# Adding data to the queue
 	def addData(self,data):
 		self.queue.put(data)
 		self.empty = self.queue.empty()
 
+	# Adding data to video queue
 	def addVideoData(self,data):
 		self.videoQueue.put(data)
 		self.videoEmpty = self.videoQueue.empty()
 		self.currentVideoFrameSize += len(data)
 
+
+	# Calculating how large a video frame will be
 	def calculateVideoFrameSize(self):
 		self.currentVideoFrameSize = 0
+		# Loops through video queue, gets qsize (a thing from the GUI library)
 		for idx in range(self.videoQueue.qsize()):
 			data = self.getVideoData()
 			self.currentVideoFrameSize += len(data)
 			self.addData(data)
 
+
+	# Getting video data
 	def getVideoData(self):
+		# Making sure that the video queue is not empty
 		if not self.videoQueue.empty():
+			# Pops the current item from the queue
 			data = self.videoQueue.get()
+			# Removes the size of the data from the frame size
 			self.currentVideoFrameSize -= len(data)
 			return data
 
 		self.videoEmpty = self.videoQueue.empty()
 
+	# Loading frames within frames, essentially the video screen and the other parts
 	def loadSubFrames(self,ctrl,diag,disp):
 		self.disp = disp
 		self.disp.grid(row=0, column=1)
@@ -63,30 +76,37 @@ class MainPage(ttk.Frame):
 		ctrl.load()
 		disp.load("forklift600.jpg", self, READSIZE)
 
+	# Updates the image within hte subframe for video
+	# This is what changes the forklift picture to something else
 	def updateImage(self, loadedImage):
 		if self.imageLabel == None:
 			self.imageLabel = ttk.Label(self, image=loadedImage).grid(column=1,row=0)
 			print ('reloading fully')
 		else:
 			self.imageLabel.configure(image = loadedImage)
-
+	
+	# Get data from the queue
 	def getData(self):
 		if not self.queue.empty():
 			return self.queue.get()
 		self.empty = self.queue.empty()
 
+	# Checks if queue is empty
 	def checkEmpty(self):
 		return self.queue.empty()
 
+	# Adds data to the queue,
 	def queueData(self, data):
 		self.sendQueue.put(data)
 		self.sendEmpty = self.sendQueue.empty()
 
+	# returns data queue if it isn't empty
 	def sendData(self):
 		if not self.sendQueue.empty():
 			return self.sendQueue.get()
 		self.sendEmpty = self.sendQueue.empty()
 
+# The control part of the GUI
 class ControlPage(ttk.Frame):
 	def __init__(self, *args, **kwargs):
 		super().__init__(**kwargs)
@@ -106,6 +126,7 @@ class ControlPage(ttk.Frame):
 		self.units = None
 		self.timeLabel = None
 
+	# TODO: Add functionality
 	def save(self):
 		print ('save file')
 
@@ -115,6 +136,8 @@ class ControlPage(ttk.Frame):
 	def stop(self):
 		print ('stop simulation')
 
+
+	# Sets time, displays according to correct unit
 	def setTime(self, time):
 		self.time = time
 		if time < 300:
@@ -129,6 +152,7 @@ class ControlPage(ttk.Frame):
 		if not self.timeLabel == None:
 			self.timeLabel['text'] = 'Runtime: ' + self.displayTime + self.units
 
+	# Sets perams for orbital stuff, just passes in all the variables
 	def setOrbitalParameters(self, Eccentricity, SemiMajorAxis, Inclination, LongitudeAscending, ArgumentPeriapsis, TrueAnomaly):
 		self.Eccentricity.set(Eccentricity)
 		self.SemiMajorAxis.set(SemiMajorAxis)
@@ -137,11 +161,13 @@ class ControlPage(ttk.Frame):
 		self.ArgumentPeriapsis.set(ArgumentPeriapsis)
 		self.TrueAnomaly.set(TrueAnomaly)
 
+	# Sets given attribute
 	def setAttitude(self, attx, atty, attz):
 		self.attitudeX.set(attx)
 		self.attitudeY.set(atty)
 		self.attitudeZ.set(attz)
 
+	# Leads the GUI itself, and eventually values?
 	def load(self):
 		# Left Buttons
 		ttk.Button(self, text="Save", command=self.save).grid(column=0, row=0, sticky=W)
@@ -174,6 +200,7 @@ class ControlPage(ttk.Frame):
 		ttk.Label(self, text="").grid(column=0, row=14) # Spacer
 		self.timeLabel = ttk.Label(self, text="Runtime: 0 seconds").grid(column=0, row=15, columnspan=3)
 
+# Diagnositcs page class
 class DiagnosticsPage(ttk.Frame):
 	def __init__(self, *args, **kwargs):
 		super().__init__(**kwargs)
@@ -184,6 +211,8 @@ class DiagnosticsPage(ttk.Frame):
 		self.powerDraw = ttk.Label(self, text="Power Draw = 0 W").grid(column=1, row=1, sticky=W)
 		self.voltage = ttk.Label(self, text="Battery Voltage = 0 V").grid(column=2, row=1)
 		self.chargePercent = ttk.Label(self, text="Estimated Charge = 0 %").grid(column=2, row=0, sticky=E)
+
+	# Setter functions for all Diagnositcs
 
 	def setMode(self, mode):
 		if not self.mode == None:
@@ -209,6 +238,7 @@ class DiagnosticsPage(ttk.Frame):
 		if not self.chargePercent == None:
 			self.chargePercent['text'] = 'Estimated Charge = ' + str(chargePercent) + ' %'
 
+# DisplayPage class
 HEIGHT, WIDTH = 600, 600
 class DisplayPage(ttk.Frame):
 	def __init__(self, *args, **kwargs):
@@ -217,6 +247,7 @@ class DisplayPage(ttk.Frame):
 		self.image = None
 		self.imageLabel = None
 
+	# Loads the initial state of the GUI
 	def load(self, filename, main, frameSize):
 		placeholder = Image.open(filename)
 		self.image = ImageTk.PhotoImage(placeholder)
@@ -226,7 +257,8 @@ class DisplayPage(ttk.Frame):
 		else:
 			self.imageLabel.configure(image = self.image)
 		disp.after(30, self.loadImage, main, frameSize)
-
+	
+	# Loads an image, after unserilizing and reserilizing, Image manipulation can happen here as well
 	def loadImage(self, main, frameSize):
 		if main.currentVideoFrameSize >= frameSize:
 			img = bytearray()
@@ -247,9 +279,9 @@ class DisplayPage(ttk.Frame):
 					h,w = 600,600
 					bpc = h*w
 					# Make a Numpy array for each channel's pixels
-					R = np.ndarray.transpose(np.frombuffer(img, dtype=np.uint8, count=bpc).reshape((h,w)))
-					G = np.ndarray.transpose(np.frombuffer(img, dtype=np.uint8, count=bpc, offset=bpc).reshape((h,w)))
-					B = np.ndarray.transpose(np.frombuffer(img, dtype=np.uint8, count=bpc, offset=2*bpc).reshape((h,w)))
+					R = np.frombuffer(img, dtype=np.uint8, count=bpc).reshape((h,w))  
+					G = np.frombuffer(img, dtype=np.uint8, count=bpc, offset=bpc).reshape((h,w))  
+					B = np.frombuffer(img, dtype=np.uint8, count=bpc, offset=2*bpc).reshape((h,w))
 
 					# Interleave the pixels from RRRRRRGGGGGGBBBBBB to RGBRGBRGBRGBRGB
 					RGB = np.dstack((R,G,B))
@@ -278,6 +310,7 @@ class DisplayPage(ttk.Frame):
 #			self.currentVideoFrame = Image.frombytes("RGB",[600,600],bytes(img))
 #			self.image = ImageTk.PhotoImage(self.currentVideoFrame)
 
+	# Loading the image the hard way
 	def loadImageManually(self, data):
 		self.currentVideoFrame = Image.frombytes("RGB",[600,600],data)
 		self.image = ImageTk.PhotoImage(self.currentVideoFrame)
@@ -288,19 +321,26 @@ class DisplayPage(ttk.Frame):
 			self.imageLabel.configure(image = self.image)
 
 #Listener pulls in data from MATLAB
+# 'localhost' should be changed to whatever address that needs to be accessed, has the port 50007
 HOST, DATAPORT = 'localhost', 50007
 def listener(main):
+	# Setting up socket, binding to address, listening for one connection
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind((HOST, DATAPORT))
 	s.listen(1)
+	# Accept connectoin
 	conn, addr = s.accept()
+	# Prints connecting address
 	print ('Connected by ', addr)
 	while 1:
 		data = conn.recv(256)
 		if not data: break
+		# If this returns anything other than 256, bad things happen
 		print ('Received data of size ', len(data))
+		# Adds in data recived from connection
 		main.addData(data)
 		main.queueData(data)
+		# Sends all data in sendData queue
 		while not main.sendEmpty:
 			sendData = main.sendData()
 			if not sendData == None:
@@ -312,13 +352,19 @@ def listener(main):
 #VideoListener pulls in video data specifically from MATLAB
 VIDEOPORT = 50080
 READSIZE = 1080000
+
+# Listening for video data
 def videoListener(main):
+	# Setting up socket, binding, listening for 1 connection
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind((HOST, VIDEOPORT))
 	s.listen(1)
+	# Accept connection
 	conn, addr = s.accept()
+	# Prints connecting address
 	print ('Connected by ', addr, ' for video')
 	totalSize = 0;
+	# Reads given data
 	while 1:
 		data = conn.recv(READSIZE)
 		if not data: break
@@ -344,6 +390,8 @@ def dataDecoder(main):
 				main.diag.setVoltage(float(batv))
 				main.diag.setChargePercent(float(batc))
 
+
+# General GUI variables
 root = Tk()
 root.title("EagleSat Simulation Interface")
 
