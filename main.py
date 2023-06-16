ddf5e5d7e181a88a32aad69e9834caee423088e6
BlueFlame <73348803+spinnerSAA@users.noreply.github.com>
2023-06-15 17:25:50 -0700

    Added driver main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+"""
+Main function for the HWIL GUI
+"""
+
+# Tkinter imports
+from tkinter import Tk
+
+# Other pages imports
+from MainPage import MainPage
+from ControlPage import ControlPage
+from DiagnosticsPage import DiagnosticsPage
+from DisplayPage import DisplayPage
+
+root = Tk()
+
+root.title("EagleSat Sim Interface")
+
+main = MainPage(root)
+
+main.grid()
+root.wm_geometry("1000x800")
+
+ctrl = ControlPage(main)
+diag = DiagnosticsPage(main)
+disp = DisplayPage(main)
+
+main.loadSubFrames(ctrl, diag, disp)
+
+root.mainloop()
